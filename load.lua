82a64b2d97f9138ccf0a2ab5efecd6ed4645084e
kaue leivas <kaueleivas0@gmail.com>
2024-05-31 19:25:49 -0300

    before backup of my computer

--- load.lua
+++ load.lua
@@ -1,40 +1,28 @@
 load = {}
 
 function load.loadAll(arg)
-    print(arg[2])
     if arg ~= nil then
         if arg[2] ~= nil then
             Filename = arg[2]
         elseif arg[1] ~= "." then
             Filename = arg[1]
-        else
-            Filename = "testFile.txt"
         end
-    end
-
-    if Filename == nil then
-        Filename = "testFile.txt" -- substitute later for a blank stencil that can create a new file
     end
 
 
 
     function love.load()
-
-        File = io.open(Filename)
+        if Filename ~= nil then
+            File = io.open(Filename) 
+        end
+        
+        
+        -- variaveis estaticas
         Font = "assests/Fonts/SourceCodePro-Italic-VariableFont_wght.ttf"
 
-        -- variaveis estaticas
         love.graphics.setNewFont(Font, 25) 
 
-        love.graphics.setBackgroundColor(0,0,0)
-
-        --[[ failed to set new icon
-        local icon = love.image.newImageData("assests/imgs/icon.png")
-        if icon ~= nil then
-            sucess = love.window.setIcon(icon)
-            print(sucess)
-        end
-       ]] 
+        love.graphics.setBackgroundColor(0,0,0.5) 
 
 
         if File ~= nil then
